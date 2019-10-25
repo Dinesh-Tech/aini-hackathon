7b4009922aab3047c57922b392ee3438cb3b7394
geetika <geetika.geetika@bt.com>
2019-10-25 19:55:44 +0100

    added data to csv

--- addDataFrame.py
+++ addDataFrame.py
@@ -8,4 +8,5 @@ raw_data = {'first_name': ['Jason', 'Molly', 'Tina', 'Jake', 'Amy'],
         'postTestScore': ["25,000", "94,000", 57, 62, 70]}
 df = pd.DataFrame(raw_data, columns = ['first_name', 'last_name', 'age', 'preTestScore', 'postTestScore'])
 
+df.to_csv("score.csv")
 print(df)
